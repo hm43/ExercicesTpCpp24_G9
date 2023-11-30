f60110c7562acf9fe5423b60221f567906a19c2c
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-11-30 15:18:41 +0000

    test ajouter pour ajouter Camion

--- Serie 1/exercice5_seance1.cpp
+++ Serie 1/exercice5_seance1.cpp
@@ -28,10 +28,7 @@ public:
         cout<<"La capacite: "<<capac<<endl;
         cout<<"Le kilometrage: "<<km<<endl;
         cout<<"Le status: "<<statut<<endl;
-        
     }
-    
-    
 };
 class GestionnaireDeFlotte{
 private:
@@ -50,37 +47,38 @@ public:
         if(choix == 1){
             Camion c1;
             flotte.push_back(c1);
-        }else if(choix == 2)
-                {
-                    cout<<"Donner l'id: ";
-                    getline(cin, id);
-                    cout<<"Donner le modele: ";
-                    getline(cin, modele);
-                    Camion c2(id, modele);
-                    flotte.push_back(c2);
-                
-    }else if(choix == 3)
-                {
-                    cout<<"Donner l'id: ";
-                    getline(cin, id);
-                    cout<<"Donner le modele: ";
-                    getline(cin, modele);
-                    cout<<"Donner capacite en tonne: ";
-                    cin>>capac;
-                    cout<<"Donner kilometrage: ";
-                    cin>>km;
-                    cout<<"Liste des status"<<endl;
-                    for(int i = 0; i< 3; i++){
-                        cout<<i+1<<" "<<s[i]<<endl;
-                    }
-                    cin>>statut;
-                    Camion c3(id, modele, capac, km, s[statut - 1]);
-                    flotte.push_back(c3);
-                }
+        }else if(choix == 2){
+            cout<<"Donner l'id: ";
+            cin.ignore();
+            getline(cin, id);
+            cout<<"Donner le modele: ";
+            getline(cin, modele);
+            Camion c2(id, modele);
+            flotte.push_back(c2);
+        }else if(choix == 3){
+            cout<<"Donner l'id: ";
+            cin.ignore();
+            getline(cin, id);
+            cout<<"Donner le modele: ";
+            getline(cin, modele);
+            cout<<"Donner capacite en tonne: ";
+            cin>>capac;
+            cout<<"Donner kilometrage: ";
+            cin>>km;
+            cout<<"Liste des status"<<endl;
+            for(int i = 0; i< 3; i++){
+                cout<<i+1<<" "<<s[i]<<endl;
+            }
+            cin>>statut;
+            Camion c3(id, modele, capac, km, s[statut - 1]);
+            flotte.push_back(c3);
+        }
         
     }
     
 };
 int main() {
+    GestionnaireDeFlotte g;
+    g.ajouterCamion();
     return 0;
 }
