07cee2e0edd797f1b820d60505d6f2288d5a6e47
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-12-21 13:58:32 +0000

    solutionexercice 1 ameliorer et exercice 3

--- Serie 2/exercice1.cpp
+++ Serie 2/exercice1.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 class Livre{
     private:
-        string titre;
-        string auteur;
-        static int nombreExemplaires;
+    string titre;
+    string auteur;
+    static int nbrexemplaires;
     public:
-        Livre(){
-            titre = "";
-            auteur = "";
-            nombreExemplaires++;
-        }
-        Livre(string titre, string auteur){
-            this->titre = titre;
-            this->auteur = auteur;
-            nombreExemplaires++;
-        }
-        Livre& operator+(const Livre &l){
-            
-        }
-
+    Livre(){
+        titre = "";
+        auteur = "";
+        nbrexemplaires++;
+    }
+    Livre(string titre, string auteur){
+        this->titre = titre;
+        this->auteur = auteur;
+        nbrexemplaires++;
+    }
+    static void afficherNombreExemplaires(){
+        cout<<"Le nombre total est: "<<nbrexemplaires<<endl;
+    }
 };
+int Livre::nbrexemplaires = 0;
 int main(){
+    Livre l1;
+    Livre l2("un titre", "un auteur");
+    Livre::afficherNombreExemplaires();
     return 0;
 }
